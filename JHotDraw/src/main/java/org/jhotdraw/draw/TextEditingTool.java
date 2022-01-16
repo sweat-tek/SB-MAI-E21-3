e4e945e90e884dad4ff3194c0d59dbe9cc335cc9
paranoidandroid07734 <90757988+paranoidandroid07734@users.noreply.github.com>
2022-01-16 20:26:23 +0100

    Update TextEditingTool.java

--- JHotDraw/src/main/java/org/jhotdraw/draw/TextEditingTool.java
+++ JHotDraw/src/main/java/org/jhotdraw/draw/TextEditingTool.java
@@ -1,16 +1,4 @@
-/*
- * @(#)TextEditingTool.java  1.0  2009-04-16
- *
- * Copyright (c) 2009 by the original authors of JHotDraw
- * and all its contributors.
- * All rights reserved.
- *
- * The copyright of this software is owned by the authors and  
- * contributors of the JHotDraw project ("the copyright holders").  
- * You may not use, copy or modify this software, except in  
- * accordance with the license agreement you entered into with  
- * the copyright holders. For details see accompanying license terms. 
- */
+
 package org.jhotdraw.draw;
 
 import java.awt.*;
@@ -27,8 +15,6 @@ import org.jhotdraw.util.ResourceBundleUtil;
  * @see TextHolderFigure
  * @see FloatingTextField
  *
- * @author Werner Randelshofer
- * @version 1.0 2009-04-16 Derived from TextTool.
  */
 public class TextEditingTool extends AbstractTool implements ActionListener {
 
